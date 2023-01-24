864ef4023c194647baa1952f1530066c027b5cd4
yanpepik <yan.pepik2001@gmail.com>
2023-01-24 18:19:41 +0100

    Setup project

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Yan Pepik on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
