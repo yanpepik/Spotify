ab43b21f530f0a784b9141a9504980b85ab0c84b
yanpepik <yan.pepik2001@gmail.com>
2023-02-06 15:46:15 +0100

    add network layer and implement Sotify SDK

---
+++ Spotify/Types/HTTPMethods.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPMethods.swift
+//  Spotify
+//
+//  Created by Yan Pepik on 06/02/2023.
+//
+
+import Foundation
+
+enum HTTPMethods: String {
+
+    case get = "GET"
+    case head = "HEAD"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case connect = "CONNECT"
+}
